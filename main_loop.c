b1a576d14f77bf4d3eef0c47ee72e56a9aa962fe
assma-codes <assma.hm89@gmail.com>
2023-11-10 06:22:38 -0800

    main loop update

--- main_loop.c
+++ main_loop.c
@@ -1,35 +1,24 @@
 #include "main.h"
 
 /**
-<<<<<<< HEAD
  * main_loop - the function which runs the shell forever until it is exited.
  * Return: no return value
  */
 
-void  main_loop()
-=======
- * main_loop - the infinite loop of the program.
- */
-void main_loop()
->>>>>>> 8b097b0f719d673bdadd38dffe5b49bb165014f3
+void  main_loop(char** command_args)
 {
-	char *line = NULL;
+	//char *line;
 	int i;
-	//char **command_args = NULL;
+	//command_args = NULL;
 
 	while (1)
 	{
-		line = printing_prompt();
-<<<<<<< HEAD
-		//command_args = parsing_command(line);
-		printf("this is the full command :%s\n", line);
+		//line = printing_prompt();
+		char** line = printing_prompt();
+		for (i = 0; i < 3; i++)
+		{
+		printf("in the main loop tokens are:%s\n", line[i]);
+		}
 	}
-	//printf("this is the full command :%s\n", line);
-	free(line);
-=======
-		command_args = parsing_command(line); 
-        }
-	free(line);
-	free(command_args);
->>>>>>> 8b097b0f719d673bdadd38dffe5b49bb165014f3
+		//command_args = parsing_command(line);
 }
