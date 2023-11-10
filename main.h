6fc378acf7e82513c85d30ee5c75e617d1a2cf7d
assma-codes <assma.hm89@gmail.com>
2023-11-09 16:43:42 -0800

    header file

--- main.h
+++ main.h
@@ -1,12 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-<<<<<<< HEAD
-/* used Standard Libraries */
-
-=======
 /* Standard Libraries */
->>>>>>> 8b097b0f719d673bdadd38dffe5b49bb165014f3
+
 #include <stdio.h>
 #include <stdarg.h>
 #include <string.h>
@@ -18,18 +14,12 @@
 #include <sys/stat.h>
 #include <signal.h>
 
-<<<<<<< HEAD
-/* functions prototypes*/
+/* simple shell functions prototypes*/
 
 char* printing_prompt(void);
 char** parsing_command(char* command_line);
 void main_loop(void);
-=======
-/*simple shell functions prototypes*/
-char *printing_prompt();
-void main_loop();
-char **parsing_command(char *command_line);
->>>>>>> 8b097b0f719d673bdadd38dffe5b49bb165014f3
+
 
 /* Macros */
 #define COMMAND_BUFSIZE 1024
