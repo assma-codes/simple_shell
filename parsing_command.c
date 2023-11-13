247cf702de7da4790419f6b42fc7ea131a1b573a
assma-codes <assma.hm89@gmail.com>
2023-11-13 03:45:18 -0800

    parsing update

--- parsing_command.c
+++ parsing_command.c
@@ -35,39 +35,14 @@ char **parsing_command(char *command_line)
 	token = strtok(command_line_copy, delimiter);
 	for (i = 0; token != NULL; i++)
 	{
-		array_of_tokens[i] = malloc(sizeof(char) * strlen(token));
+		array_of_tokens[i] = malloc(sizeof(char) * (strlen(token) + 1));
 		strcpy(array_of_tokens[i], token);
-		printf("%s\n", array_of_tokens[i]);
+		/*printf("%s\n", array_of_tokens[i]);*/
 		token = strtok(NULL, delimiter);
 	}
 	array_of_tokens[i] = NULL;
 	free(command_line_copy);
 	return array_of_tokens;
-	free(array_of_tokens);
-	//return array_of_tokens;
 }
-/*
-int main(int ac, char** argv)
-{
-	//char argv** =NULL;
-	char *shell_prompt = "MASS$";
-        char *command_line = NULL;
-        size_t n_of_chars = 0;
-	
-	while (1)
-	{
-        //write(1, &shell_prompt, strlen(shell_prompt));
-        printf("%s", shell_prompt);
-        if(getline(&command_line, &n_of_chars, stdin) == -1)
-        {
-                exit(EXIT_FAILURE);
-        }
-        else if (feof(stdin))
-        {
-                exit(EXIT_SUCCESS);
-        }
-	char** argv = parsing_command(command_line);
-	}
-        free(command_line);
-}*/
+
 
