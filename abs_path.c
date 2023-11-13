c924eca557b1a49d33d8fa28cd5b447b1d3a92ea
Mohd-abdallah <dawry798@gmail.com>
2023-11-13 14:37:34 +0200

    fixing Errors and bettty style

--- abs_path.c
+++ abs_path.c
@@ -6,9 +6,9 @@
  * Return: absolute path of the comand
  */
 
-char* abs_path(char* command)
+char *abs_path(char *command)
 {
-	char *full_path, *full_path_copy,*file_path,* path_token;
+	char *full_path, *full_path_copy, *file_path, *path_token;
 	struct stat buf;
 	char command_length, directory_length;
 
@@ -16,25 +16,25 @@ char* abs_path(char* command)
 	full_path = getenv("PATH");
 	if (full_path)
 	{
-	full_path_copy = strdup(full_path);
-	path_token = strtok(full_path_copy, ":");
-	while (path_token != NULL)
-	{
-	directory_length = strlen(path_token);
-	file_path = malloc(command_length + directory_length + 2);
-	strcpy(file_path, path_token);
-	strcat(file_path, "/");
-	strcat(file_path, command);
-	strcat(file_path, "\0");
-	if (stat(file_path, &buf) == 0)
-	{
-		return file_path;
-		free(file_path);
-	}
-	else
-		free(file_path);
-	path_token = strtok(NULL, ":");
-	}
+		full_path_copy = strdup(full_path);
+		path_token = strtok(full_path_copy, ":");
+		while (path_token != NULL)
+		{
+			directory_length = strlen(path_token);
+			file_path = malloc(command_length + directory_length + 2);
+			strcpy(file_path, path_token);
+			strcat(file_path, "/");
+			strcat(file_path, command);
+			strcat(file_path, "\0");
+			if (stat(file_path, &buf) == 0)
+			{
+				return (file_path);
+				free(file_path);
+			}
+			else
+			free(file_path);
+			path_token = strtok(NULL, ":");
+		}
 	}
 	free(full_path_copy);
 	if (stat(command, &buf) == 0)
@@ -43,18 +43,3 @@ char* abs_path(char* command)
 	}
 	return (NULL);
 }
-/*
-int main()
-{
-	char *command = "grep";
-	char *full_command_path = abs_path(command);
-	if(full_command_path)
-	{
-	printf("this is the full path : %s\n", full_command_path);
-	}
-	else
-	{	
-		printf("%s command not found\n", command);
-	}
-	return (0);
-}*/
