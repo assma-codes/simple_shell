7808d7a6e6eac402deb5bcabf9e4faf4481201b8
Mohd-abdallah <dawry798@gmail.com>
2023-11-13 20:50:09 +0200

    Fixing Betty style errors

--- executing_command.c
+++ executing_command.c
@@ -10,33 +10,35 @@ void executing_command(char **argv)
 {
 	pid_t child_proccess;
 
-	if(argv && argv[0])
+	if (argv && argv[0])
 	{
 		char *complete_command = argv[0];
 		char *excat_command = abs_path(complete_command);
+
 		if (excat_command != NULL)
 		{
-				child_proccess = fork();
-				if (child_proccess == 0)
+			child_proccess = fork();
+			if (child_proccess == 0)
+			{
+				if (execve(excat_command, argv, NULL) == -1)
 				{
-					if (execve(excat_command, argv, NULL) == -1)
-		{
-			perror("Error executing command");
-		}
-		}
-		else if (child_proccess > 0)
-		{
-			int status;
-			wait(&status);
-		}
+					perror("Error executing command");
+				}
+			}
+			else if (child_proccess > 0)
+			{
+				int status;
+
+				wait(&status);
+			}
 		}
 		else
 		{
 			printf("Command not found: %s\n", complete_command);
 		}
 	}
-		else
-		{
-			printf("Invalid command\n");
-		}
+	else
+	{
+		printf("Invalid command\n");
+	}
 }
